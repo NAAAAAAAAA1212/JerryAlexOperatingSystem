7874f3543d65060598e7b217abee3e365514b963
Zeng Alexandre Qizhi <alexa@microsoft.zengqizhi.eu.org>
2024-01-05 17:46:37 +0800

    creating calc.py

---
+++ core/ext/calc.py
@@ -0,0 +1,23 @@
+def plus(n1, n2):
+    try:
+        return n1 + n2
+    except:
+        return
+
+def minu(n1, n2):
+    try:
+        return n1 + n2
+    except:
+        return
+    
+def devi(n1, n2):
+    try:
+        return n1 + n2
+    except:
+        return
+
+def mult(n1, n2):
+    try:
+        return n1 + n2
+    except:
+        return
