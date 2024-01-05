3bd7ec1f71dc52915683ae7ef54d6b8cf54f2516
Zeng Alexandre Qizhi <alexa@microsoft.zengqizhi.eu.org>
2024-01-05 17:35:50 +0800

    version name update sketching gui entry for gui

--- core/load.py
+++ core/load.py
@@ -1,7 +1,9 @@
 #init
+gui = True
 boot = True
 i1 = True
 i2 = True
+out = 0
 
 #testing
 try:
@@ -17,8 +19,8 @@ except:
 if i1 == False:
     print("module OS not found, will not boot")
 if i2 == False:
-    print("module easygui not found, will not boot")
-    boot = False
+    print("module easygui not found, GUI will be disabled")
+    gui = False
 
 #boot
 if boot == True:
@@ -60,13 +62,72 @@ if boot == True:
             print("\033[1mLanguage: 語言: ")
             print("\033[0m\033[1m1: English")
             print("\033[1;30;47m2: 繁體中文\033[0m\033[1m")
-    if lang == "1":
+    if gui == True:
+        print("1 = Up ; 2 = Down ; 3 = Enter")
+        print("\033[1mGUI:")
+        print("\033[1;30;47m1: DOS")
+        print("\033[0m\033[1m2: Run On Dos with popup")
+        print("\033[0m\033[1m2: GUI")
+        lang = "1"
+        max = 2
+        limit = 0
+        num = 0
+        while 1 == 1:
+            key = input()
+            if key == "3":
+                out = str(num + 1)
+                break
+            if key == "1":
+                num = num - 1
+            if key == "2":
+                num = num + 1
+            if num < limit:
+                num = limit
+            if num > max:
+                num = max
+            os.system(clear)
+            if num == 0:
+                print("\033[1mGUI:")
+                print("\033[0m\033[1m1;30;47m1: DOS")
+                print("\033[0m\033[1m2: Run On Dos with popup")
+                print("\033[0m\033[1m2: GUI")
+            if num == 1:
+                print("\033[1mGUI:")
+                print("\033[0m\033[1m1: DOS")
+                print("\033[1;30;47m\033[1m2: Run On Dos with popup")
+                print("\033[0m\033[1m2: GUI")
+            if num == 2:
+                print("\033[1mGUI:")
+                print("\033[0m\033[1m1: DOS")
+                print("\033[0m\033[1m2: Run On Dos with popup")
+                print("\033[1;30;47m2: GUI")
+    if lang == "1" and out == "0":
         if os.path.exists("core/eng.py") == True:
             os.system("python core/eng.py")
         else:
-            print("lang file doesn't exsist! Please sel other lang!")
-    if lang == "2":
+            print("File doesn't exsist!")
+    if lang == "2" and out == "0":
         if os.path.exists("core/chi.py") == True:
             os.system("python core/chi.py")
         else:
-            print("語言文件不存在! 請選擇其他語言!")
+            print("文件不存在!")
+    if lang == "1" and out == "1":
+        if os.path.exists("core/engg1.py") == True:
+            os.system("python core/engg1.py")
+        else:
+            print("File doesn't exsist!")
+    if lang == "2" and out == "1":
+        if os.path.exists("core/chig1.py") == True:
+            os.system("python core/chig1.py")
+        else:
+            print("文件不存在!")
+    if lang == "1" and out == "2":
+        if os.path.exists("core/engg2.py") == True:
+            os.system("python core/engg2.py")
+        else:
+            print("File doesn't exsist!")
+    if lang == "2" and out == "2":
+        if os.path.exists("core/chig2.py") == True:
+            os.system("python core/chig2.py")
+        else:
+            print("文件不存在!")
