2286c307468b50808623ad391fd9894033ffcb8e
Zeng Alexandre Qizhi <alexa@microsoft.zengqizhi.eu.org>
2024-02-17 11:10:33 +0800

    adding app u r a idiot

---
+++ core/apps/u_r_a_idiot.py
@@ -0,0 +1,6 @@
+try:
+    import easygui
+    while (True):
+        easygui.buttonbox(msg = "YOU ARE A IDIOT!", title = "YOU ARE A IDIOT!", choices = ("I AM A IDIOT!", "I AM A IDIOT!", "I AM A IDIOT!", "I AM A IDIOT!", "I AM A IDIOT!"), )
+except:
+    print("naaa u don't have a UI")
